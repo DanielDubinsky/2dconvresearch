d3effa80dd898b712023d781228f3c1f72fab086
Daniel Dubinsky <danield95@gmail.com>
2020-05-30 18:30:07 +0300

    MovingMnist: save mean,std for train_ratio = 0.9

--- datasets/MovingMNIST.py
+++ datasets/MovingMNIST.py
@@ -10,15 +10,26 @@ class MovingMNIST(Dataset):
         np_arr = np.load(np_arr_path)
 
         self.images = torch.from_numpy(np_arr)
+        self.images = self.images.unsqueeze(2)
+
+        train_ratio = 0.9
 
         num_sequences = self.images.size(1)
-        num_train = int(0.8 * num_sequences)
-        if train:
-            self.images = self.images[:, num_train:]
-        else:
-            self.images = self.images[:, : num_train]
+        num_train = int(train_ratio * num_sequences)
 
-        self.images = self.images.unsqueeze(2)
+        self.train_images = self.images[:, : num_train]
+        self.test_images = self.images[:, num_train:]
+        if train:
+            self.images = self.train_images
+        else:
+            self.images = self.test_images
+
+        if train_ratio == 0.9:
+            self.mean = 12.567464828491211
+            self.std = 51.07254409790039
+        else:
+            self.mean = torch.mean(self.train_images.to(dtype=torch.float32))
+            self.std = torch.std(self.train_images.to(dtype=torch.float32))
 
     def __len__(self):
         return self.images.size(1)
